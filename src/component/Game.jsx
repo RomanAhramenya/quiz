8af7f62ce4e288b3370590346e8cee911672cab3
Roman <romanahramenya@gmail.com>
2022-05-24 04:20:25 +0300

    delete classActive

--- src/component/Game.jsx
+++ src/component/Game.jsx
@@ -5,16 +5,13 @@ import { QuizContext } from '../helpers/QuizContext'
 function Game() {
     const {setPage,score,setScore} = useContext(QuizContext)
     const [currentQuestion,setCurrentQustion] = useState(0)
-    const [active,setActive] = useState(0)
     const [choice,setChoce] = useState("")
-    function choiceAnswer(variant,active){
+    function choiceAnswer(variant){
         setChoce(variant)
-        setActive(active)
     }
     function nextHandler() {
         if (Questios[currentQuestion].answer === choice){
             setScore(score +1)
-            setActive(0)
         }
         
         setCurrentQustion(currentQuestion + 1)
@@ -29,10 +26,10 @@ function Game() {
   return (
     <div className='Game'>
         <h1>{Questios[currentQuestion].prompt}</h1>
-        <button className={active === 1 ? 'active' : ''} onClick={()=>choiceAnswer('optionA',1)}>{Questios[currentQuestion].optionA}</button>
-        <button className={active === 2 ? 'active' : ''} onClick={()=>choiceAnswer('optionB',2)}>{Questios[currentQuestion].optionB}</button>
-        <button className={active === 3 ? 'active' : ''} onClick={()=>choiceAnswer('optionC',3)}>{Questios[currentQuestion].optionC}</button>
-        <button className={active === 4 ? 'active' : ''} onClick={()=>choiceAnswer('optionD',4)}>{Questios[currentQuestion].optionD}</button>
+        <button onClick={()=>choiceAnswer('optionA')}>{Questios[currentQuestion].optionA}</button>
+        <button onClick={()=>choiceAnswer('optionB')}>{Questios[currentQuestion].optionB}</button>
+        <button onClick={()=>choiceAnswer('optionC')}>{Questios[currentQuestion].optionC}</button>
+        <button onClick={()=>choiceAnswer('optionD')}>{Questios[currentQuestion].optionD}</button>
        {currentQuestion === Questios.length - 1 ? <button onClick={finish}>Finish</button> :
        <button onClick={nextHandler}>Next Questios</button>}
         
